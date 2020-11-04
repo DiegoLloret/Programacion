2aea4145ab977cc838f866afb455af76a2738c55
Alumno 3 <Alumno 3@DESKTOP-LEUAO1L.localdomain>
2020-11-04 08:38:09 +0100

    clase

---
+++ Programacion/src/UT3/ejercicio14.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package UT3;
+
+/**
+ * @author Alumno 3
+ *
+ */
+import java.util.Scanner;
+
+public class ejercicio14 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("introduce precio inicial ");
+		Scanner precio=new Scanner(System.in);
+		int precio1= precio.nextInt();
+		int numero1 = 0;
+		if( numero1 ==0)
+		{
+			System.out.println( precio+ "=> insuficiente");
+		}
+		
+	}
+
+}
---
+++ Programacion/src/UT3/ejercicio15.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package UT3;
+
+/**
+ * @author Alumno 3
+ *
+ */
+import java.util.Scanner;
+public class ejercicio15 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("introduce tu nota ");
+		Scanner nota=new Scanner(System.in);
+		
+		float nota1=nota.nextInt(); 
+		if( nota1 >=0 && nota1 <5)
+		{
+			System.out.println(nota1 + "=> insuficiente");
+		}
+		else if(nota1 <6)
+		{
+			System.out.println(nota1 + "=> suficiente");
+		}
+		else if(nota1 <7)
+			{
+				System.out.println(nota1 + "=> bien");	
+			}
+	
+		else if(nota1 <=8)
+		{
+		System.out.println(nota1 + "=> notable");
+		}
+		else if(nota1 <=10)
+		{
+			System.out.println(nota1 + "=> sobresaliente");
+		}
+		else if(nota1 >10)
+		{
+		System.out.println(nota1 + "=> error");
+		}
+		else if(nota1 <0)
+		{
+		System.out.println(nota1 + "=> suficiente");
+		}
+				
+	}
+	
+}
