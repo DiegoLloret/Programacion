5cbc3aa036c8e87c50c310392947765bfe52d1b1
f009979n <f009979n@MSCDIT00N.home>
2020-11-04 18:07:46 +0100

    casa 1.1

---
+++ programacion/src/UT3/ejercicio2.java
@@ -0,0 +1,75 @@
+/**
+
+ * 
+
+ */
+
+package UT3;
+
+
+
+/**
+
+ * @author Alumno 3
+
+ * decir si un numero es par o impar.
+
+ */
+
+public class ejercicio2 {
+
+
+
+	/**
+
+	 * @param args
+
+	 */
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+	    int numero = 9; 
+
+
+
+	     
+
+
+
+	    if (numero%2==0) 
+
+
+
+	    { 
+
+
+
+	       System.out.println("es parr"); 
+
+
+
+	         
+
+
+
+	    } else { 
+
+
+
+	    	System.out.println ("es impar"); 
+
+
+
+	    } 
+
+
+
+	     
+
+
+
+	} 
+
+	}
---
+++ programacion/src/UT3/ejercicio3.java
@@ -0,0 +1,72 @@
+/**
+
+ * 
+
+ */
+
+package UT3;
+
+
+
+/**
+
+ * @author Alumno 3
+
+ *
+
+ */
+
+import java.util.Scanner;
+
+
+
+public class ejercicio3 {
+
+
+
+	/**
+
+	 * @param args
+
+	 */
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		Scanner entrada = new Scanner(System.in);
+
+		String cadena = entrada.nextLine();
+
+		
+
+        int A = entrada.nextInt();
+
+        int B = entrada.nextInt();
+
+        int C = entrada.nextInt();
+
+        int aux = entrada.nextInt();
+
+	
+
+		
+
+		System.out.println("Escribe un numero");
+
+		System.out.println("Escribe un numero");
+
+		System.out.println("Escribe un numero");
+
+
+
+		
+
+	}
+
+
+
+	
+
+	}
+
