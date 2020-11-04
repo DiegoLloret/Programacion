832ba86a84ab0b34a220a5358a471812be30f59f
Alumno 3 <Alumno 3@DESKTOP-LEUAO1L.localdomain>
2020-11-04 12:18:28 +0100

    final clase

---
+++ Programacion/src/UT3/ejercicio22.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package UT3;
+
+import java.util.Scanner;
+
+/**
+ * @author Alumno 3
+ *
+ */
+public class ejercicio22 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in); 
+	
+		 
+		  
+		
+        System.out.println("Escribe un numero"); 
+        int numero1 = entrada.nextInt();
+        System.out.println("Escribe la potencia"); 
+        int potencia = entrada.nextInt();
+    	int operacion = (numero1*numero1);
+	
+        System.out.println("La respuesta: " +operacion ); 
+		
+        entrada.close(); 
+
+	
+
+}
+}
