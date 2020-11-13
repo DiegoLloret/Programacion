b56c89e62c0ee3727565574cefbd9bc2b11d10ad
f009979n <f009979n@MSCDIT00N.home>
2020-11-13 13:44:41 +0100

    casa 13/11

---
+++ programacion/src/UT3/ejercicio37.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package UT3;
+
+import java.util.Scanner;
+
+/**
+ * @author f009979n
+ *
+ */
+public class ejercicio37 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner numero = new Scanner(System.in);
+		System.out.println("Escribe el alto");
+		int numero1=numero.nextInt();
+		System.out.println("Escribe el ancho");
+		int numero2=numero.nextInt();
+		
+		for (int i=1; i<=numero1; i++) {
+			System.out.println("");
+			for (int j=1; j<=numero2;j++)
+				System.out.printf("* ");
+				
+			
+		}
+	}
+
+}
