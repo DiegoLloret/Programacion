803cdd56b5399a12034acd7453914bf25cd49c60
Alumno 3 <Alumno 3@DESKTOP-LEUAO1L.localdomain>
2020-11-16 13:44:57 +0100

    clase 16/11

---
+++ Programacion/src/UT5/ejercicio63.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package UT5;
+
+import java.util.Scanner;
+
+/**
+ * @author Alumno 3
+ *
+ */
+public class ejercicio63 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner numero = new Scanner(System.in);
+		int numeros[];
+		numeros= new int[10];
+		int suma;
+		
+		
+		for (int i=0; i<numeros.length; i++) {
+			System.out.println("ponme el numero "+(i+1));
+			numeros[i]=numero.nextInt();
+			
+		}
+	
+			
+	}
+	}
+
