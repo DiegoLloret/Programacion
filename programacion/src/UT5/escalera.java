5bccef1d59ab2b9a7686522692c9facf3c6e3c59
f009979n <f009979n@MSCDIT00N.home>
2020-11-25 21:11:54 +0100

    casa 25/11 2.0

--- programacion/src/UT5/escalera.java
+++ programacion/src/UT5/escalera.java
@@ -22,84 +22,67 @@ public class escalera {
 		int menor=0;
 		int resta=0;
 		int [] carta = new int[5];
-		 char [] palo = new char[5];  
+		char [] palo = new char[5];  
 		while(true)
-			{
-				 for (int i=1; i<5 ;  i++)
-				 { 
-					 
-					 
-					 carta[i]=-1;
-					  while   (carta[i]<0 ||  carta[i]>13)
-					  {
-			
-						 System.out.println("dime la carta "+i+ " :");
-						 carta[i] = ent.nextInt();
-						 
-					 
-					  }
-					  if (carta[i]==0)
-					  {
-						  return;
-					  }
-					  palo[i] = 'x';
-					  while   (palo[i]!= 'c' && palo[i]!='d' && palo[i]!='t'&& palo[i]!='p')
-					  {                                                                                                                                                                                                                                                                                                                                                                            
-						 System.out.println("dime el  palo "+i+ " :");
-						 palo[i] = ent.next().charAt(0);               
-					  }
-					   	
-				 }
-				 
-				 
-				if (palo[1]==palo[2] && palo[1]==palo[3] && palo[1]==palo[4])
-				{
-					 System.out.println("los palos son iguales");
-					  igualpalo=true;
+		{
+			for (int i = 1; i < 5; i++) {
+
+				carta[i] = -1;
+				while (carta[i] < 0 || carta[i] > 13) {
+
+					System.out.println("dime la carta " + i + " :");
+					carta[i] = ent.nextInt();
+
 				}
-			
-				if ( igualpalo==false)
-				{
-					 System.out.println("No se puede hacer escalera de color.");
+				if (carta[i] == 0) {
+					return;
 				}
-				
-				else {
-					
-					//algoritmo para ver si es escalera
-					
-				
+				palo[i] = 'x';
+				while (palo[i] != 'c' && palo[i] != 'd' && palo[i] != 't' && palo[i] != 'p') {
+					System.out.println("dime el  palo " + i + " :");
+					palo[i] = ent.next().charAt(0);
+				}
+
+			}
+
+			if (palo[1] == palo[2] && palo[1] == palo[3] && palo[1] == palo[4]) {
+				System.out.println("los palos son iguales");
+				igualpalo = true;
+			}
+
+			if (igualpalo == false) {
+				System.out.println("No se puede hacer escalera de color.");
+			}
+
+			else {
+
+				// algoritmo para ver si es escalera
+
 				Arrays.sort(carta);
-				 for (int i = 1; i < carta.length; i++) 
-				 {
-					 System.out.println(carta[i]+" "+palo[i]);
-				 }
-				resta=carta[4] - carta[1];
-				 if (resta >4)
-				 {
-					 System.out.println("No se puede hacer escalera de color.");
-				 }
-				 else if (resta==3)
-				 {
-					 System.out.println("Falta un "+(carta[4]+1)+" "+palo[4]+ " para hacer escalera de color.");
-				 }
-				 else //resta=4
-				 {
+				for (int i = 1; i < carta.length; i++) {
+					System.out.println(carta[i] + " " + palo[i]);
+				}
+				resta = carta[4] - carta[1];
+				if (resta > 4) {
+					System.out.println("No se puede hacer escalera de color.");
+				} else if (resta == 3) {
+					System.out.println("Falta un " + (carta[4] + 1) + " " + palo[4] + " para hacer escalera de color.");
+				} else // resta=4
+				{
 					// System.out.println("Falta un hueco para hacer escalera de color.");
-					
-					 for (int i = 1; i < carta.length-1; i++) 
-					 {
-						
-						 if (carta[i+1]-carta[i]!=1)
-						 {
-							 
-							 System.out.println("Falta un "+(carta[i]+1)+" "+palo[i]+ " para hacer escalera de color.");
-						
-						 }
-					 }
-						 
-		
-				 }
+
+					for (int i = 1; i < carta.length - 1; i++) {
+
+						if (carta[i + 1] - carta[i] != 1) {
+
+							System.out.println(
+									"Falta un " + (carta[i] + 1) + " " + palo[i] + " para hacer escalera de color.");
+
+						}
+					}
+
+				}
+			}
 		}
-	}
 	}
 }
